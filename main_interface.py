670a47071087e94aefefbfcb35fb48951c2c95d3
Matéo <mateo.soutter@gmail.com>
2026-09-08 12:29:47 -0400

    Changement de saison

--- main_interface.py
+++ main_interface.py
@@ -495,12 +495,12 @@ elif onglet_actif == "GAMERULES":
     L'arène est une immense grille de **9 km par 9 km**, découpée en cases carrée de 300 mètres de côté à débloquer.
 
     ### 📜 Règles :
-    1. **Comment gagner :** C'est simple celui qui a débloqué le plus de tuiles à la fin de la saison gagne (voir l'onglet classements). Il suffit que votre trace GPS traverse une case pour que celle-ci soit débloquée.
+    1. **Comment gagner :** Celui ou celle qui a débloqué le plus de tuiles à la fin de la saison gagne (voir l'onglet classements). Il suffit que votre trace GPS passe 1 fois une case pour que celle-ci soit débloquée.
     2. **Le sport :** Seules la course à pied et la marche sont autorisées. Laissez les vélos au garage ! (ou payez moi et je développe la même appli en version cycliste)
     3. **La période :** La saison multijoueur actuelle se déroule sur le mois de septembre **du 1er au 30 septembre 2026**.
     4. **La zone :** Les traces qui ne débloquent aucune case de l'arène ne sont pas considérées pour alléger l'affichage et la mémoire utilisée.
     5. **Pas de double-points :** Repasser dans une case que vous avez déjà conquise ne rapporte aucun point supplémentaire. L'objectif est l'**EXPLORATION**.
-    6. **Le mode solo :** Vous pouvez créer votre propre arène et vous comparer aux autres joueurs sur votre terrain. Les règles sont les mêmes que pour le mode multijoueur avec une arène plus grande.
+    6. **Le mode solo :** Vous pouvez aussi créer votre propre arène et vous comparer aux autres joueurs sur votre terrain. Les règles sont les mêmes que pour le mode multijoueur avec une arène plus grande.
 
     ### ⚙️ Comment faire :
     1. Allez sur strava ou tout logiciel de gestion de montre GPS (garmin, suunto, coros)
