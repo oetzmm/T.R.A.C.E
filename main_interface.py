081dbda36737d50eae034b577479cf58937bf79b
Matéo <mateo.soutter@gmail.com>
2026-09-08 12:16:48 -0400

    Changement de l'enregistrement des arènes

--- main_interface.py
+++ main_interface.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import json
 import pydeck as pdk
 import math
 import time
@@ -140,7 +138,7 @@ if onglet_actif == "MULTIPLAYER MODE":
             )
             layers_m.append(layer_ephemere_m)
 
-        st.markdown("### Affichage carte multijoueur")
+        st.markdown(f"### Affichage carte multijoueur : {n_m} tuiles de {tile_length_m*1000} m de côté")
         tous_les_joueurs = list(col_multi.find({}, {"_id": 0}))
         pseudo_dispo_m = []
         for joueur in col_multi.find({}, {"pseudo": 1}): 
@@ -340,7 +338,9 @@ elif onglet_actif == "SOLO MODE":
 
             barre_prog_p.empty()
             st.success(f"Traces ajoutées ! Score temporaire : {len(st.session_state.tuiles_p)} tuiles.")
-            
+
+    st.markdown(f"### Affichage carte multijoueur : {n_m} tuiles de {tile_length_m*1000} m de côté")
+
     layers_p = []
     colored_tiles_p = set()
     colored_tiles_p.update(st.session_state.tuiles_p)
